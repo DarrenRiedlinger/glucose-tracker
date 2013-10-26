b2a496b2d7aadb0b2ecaf4087a5260bdade24fb0
jonathan <jonathan@calazan.com>
2013-10-26 02:02:20 +0200

    Initial commit.

---
+++ glucosetracker/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'glucosetracker',
+        'USER': 'developer',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+INSTALLED_APPS += ('debug_toolbar', )
+INTERNAL_IPS = ('127.0.0.1',)
+MIDDLEWARE_CLASSES += \
+    ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+
