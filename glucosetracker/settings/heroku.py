b8cc67ebc7c6c5fa2927fa9dd755534b2fb480ca
jonathan <jonathan@calazan.com>
2013-11-15 02:38:18 +0100

    Testing heroku settings.

--- glucosetracker/settings/heroku.py
+++ glucosetracker/settings/heroku.py
@@ -2,14 +2,14 @@ import os
 
 from .base import *
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
