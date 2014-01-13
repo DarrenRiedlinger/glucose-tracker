e0df5e479bed86e1c33856afe344b17758d4fbc5
jonathan <jonathan@calazan.com>
2014-01-13 17:21:49 -0500

    Updated backup script.

--- scripts/db_backup.py
+++ scripts/db_backup.py
@@ -1,7 +1,7 @@
 """
 A simple script to backup the PostgreSQL database.
 
-TODO: Add email alert if exit code is not 0.
+TODO: Add email alert if exit code is not 0 or exceptions occur.
 """
 import os
 import sys
@@ -19,10 +19,10 @@ BACKUP_PATH = r'/webapps/glucosetracker/db_backups'
 
 FILENAME_PREFIX = 'glucosetracker.backup'
 
-# Amazon S3 settings
+# Amazon S3 settings.
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-BUCKET_NAME = 'glucosetracker-db-backups'
+AWS_BUCKET_NAME = 'glucosetracker-db-backups'
 
 
 def main():
@@ -31,14 +31,14 @@ def main():
                       help="Specify either 'hourly' or 'daily'.")
 
     now = datetime.now()
-    hour = str(now.hour).zfill(2)
-    day_of_year = str(now.timetuple().tm_yday).zfill(3)
 
     filename = None
     (options, args) = parser.parse_args()
     if options.backup_type == 'hourly':
+        hour = str(now.hour).zfill(2)
         filename = '%s.h%s' % (FILENAME_PREFIX, hour)
     elif options.backup_type == 'daily':
+        day_of_year = str(now.timetuple().tm_yday).zfill(3)
         filename = '%s.d%s' % (FILENAME_PREFIX, day_of_year)
     else:
         parser.error('Invalid argument.')
@@ -61,10 +61,10 @@ def main():
 
 def upload_to_s3(source_path, destination_filename):
     """
-    Upload a file to Amazon S3.
+    Upload a file to an AWS S3 bucket.
     """
     conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-    bucket = conn.get_bucket(BUCKET_NAME)
+    bucket = conn.get_bucket(AWS_BUCKET_NAME)
     k = Key(bucket)
     k.key = destination_filename
     k.set_contents_from_filename(source_path)
