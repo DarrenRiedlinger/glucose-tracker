10343b5e26628aa61fa606c191f02feff971ba5d
jonathan <jonathan@calazan.com>
2014-03-30 18:32:03 -0400

    Updated fabfile again.

--- deployment/fabric/fabfile.py
+++ deployment/fabric/fabfile.py
@@ -42,4 +42,8 @@ def deploy():
                         '--settings=%s' % settings_file
         sudo('%s && %s' % (venv_command, south_command), user=owner)
 
+        collectstatic_command = 'python glucosetracker/manage.py collecstatic --noinput ' \
+                        '--settings=%s' % settings_file
+        sudo('%s && %s' % (venv_command, collectstatic_command), user=owner)
+
         sudo('supervisorctl restart glucosetracker')
