bcd436ba85c9961981fa3e2a93645f1b615c657c
jonathan <jonathan@calazan.com>
2014-04-12 19:22:15 -0400

    Fixed bug when converting mmol/L to mg/dL.

--- glucosetracker/core/utils.py
+++ glucosetracker/core/utils.py
@@ -55,7 +55,14 @@ def to_mg(value):
     """
     Convert a given value in mmol/L to mg/dL rounded to nearest integer.
     """
-    return int(round((float(value) * 18.018), 0))
+    try:
+        return int(round((float(value) * 18.018), 0))
+    except ValueError:
+        # We're catching ValueError here as some browsers like Firefox won't
+        # validate the input for us. We're returning the value entered as this
+        # will be passed in to the Django validator which will return the
+        # validation error message.
+        return value
 
 
 def glucose_by_unit_setting(user, value):
