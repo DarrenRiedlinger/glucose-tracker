3658ed7f4029da2b83730d1ee4621e794014bb66
jonathan <jonathan@calazan.com>
2014-04-20 17:15:57 -0400

    Added dev settings.

---
+++ glucosetracker/settings/dev.py
@@ -0,0 +1,59 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# 3rd-party apps tracking IDs.
+INTERCOM_APP_ID = None
+GOOGLE_ANALYTICS_TRACKING_ID = None
+
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+ADMINS = (
+    ('Local Admin', 'admin@glucosetracker.net'),
+)
+
+MANAGERS = ADMINS
+
+CONTACTS = {
+    'support_email': 'support@glucosetracker.net',
+    'admin_email': 'admin@glucosetracker.net',
+    'info_email': 'info@glucosetracker.net',
+}
+
+# For 'subscribers' app
+SEND_SUBSCRIBERS_EMAIL_CONFIRMATION = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'glucosetracker',
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# Django-debug-toolbar config
+INSTALLED_APPS += ('debug_toolbar',)
+INTERNAL_IPS = ('127.0.0.1', '192.168.33.1',)
+MIDDLEWARE_CLASSES += \
+    ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TEMPLATE_CONTEXT': True,
+    'HIDE_DJANGO_SQL': False,
+}
