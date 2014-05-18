f3c462dec11f1cf8b009daddd114674033359dfe
jcalazan <jcalazan@noodle.org>
2014-05-18 15:12:39 -0400

    Added header check for CSV import.

--- glucosetracker/glucoses/utils.py
+++ glucosetracker/glucoses/utils.py
@@ -27,21 +27,28 @@ def import_glucose_from_csv(user, csv_file):
 
     glucose_objects = []
 
-    # Assume first row is the header, so let's skip it.
-    for row in csv_data[1:]:
-        try:
-            category = Category.objects.get(name__iexact=row[1].strip())
-        except ObjectDoesNotExist:
-            category = Category.objects.get(name__iexact='No Category'.strip())
+    # Check if headers exists. Skip the first entry if true.
+    header_check = ['value', 'category', 'date', 'time']
+    first_row = [i.lower().strip() for i in csv_data[0]]
+    if all(i in first_row for i in header_check):
+        csv_data = csv_data[1:]
 
-        glucose_objects.append(Glucose(
-            user=user,
-            value=int(row[0]),
-            category=category,
-            record_date=datetime.strptime(row[2], DATE_FORMAT),
-            record_time=datetime.strptime(row[3], TIME_FORMAT),
-            notes=row[4],
-        ))
+    for row in csv_data:
+        # Let's do an extra check to make sure the row is not empty.
+        if row:
+            try:
+                category = Category.objects.get(name__iexact=row[1].strip())
+            except ObjectDoesNotExist:
+                category = Category.objects.get(name__iexact='No Category'.strip())
+
+            glucose_objects.append(Glucose(
+                user=user,
+                value=int(row[0]),
+                category=category,
+                record_date=datetime.strptime(row[2], DATE_FORMAT),
+                record_time=datetime.strptime(row[3], TIME_FORMAT),
+                notes=row[4],
+            ))
 
     Glucose.objects.bulk_create(glucose_objects)
 
